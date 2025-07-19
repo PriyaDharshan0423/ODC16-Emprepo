8e563d316411c22cefb12b34c538c272ff1d087e
Mancya Arokiya Mary V <mancya.arokiyamaryv@hcltech.com>
2025-07-19 12:45:25 +0530

    added a new line 2 by mancy

--- MyFirstEmpProject/src/com/hcltech/client/EmpClient.java
+++ MyFirstEmpProject/src/com/hcltech/client/EmpClient.java
@@ -4,5 +4,7 @@ public class EmpClient {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Add a new line 1");
+
+		System.out.println("Add a new line 2");
 	}
 }
