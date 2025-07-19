d1b050fd0af38d2afbfc96127531727eb3b6c91c
Priyadharshan V <priyadharshan.v@hcltech.com>
2025-07-19 12:41:03 +0530

    Added a new line by at

--- MyFirstEmpProject/src/com/hcltech/client/EmpClient.java
+++ MyFirstEmpProject/src/com/hcltech/client/EmpClient.java
@@ -3,5 +3,6 @@ package com.hcltech.client;
 public class EmpClient {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Add a new line 1");
 	}
 }
